991402240d78f676d8334936bf32c93d0ebad4d1
Robin Tirole <robin.tirole@divcom.ch>
2019-05-20 10:37:35 +0200

    Modification en-tête php

--- mon-premier-plugin.php
+++ mon-premier-plugin.php
@@ -1,4 +1,10 @@
 <?php
-/*
-Plugin Name: Mon Premier Plugin
- */
+
+/**
+* Plugin Name: Mon Premier Plugin
+* Description: Premier plugin wordpress créer par Robin Tirole afin d'exécuter des tests
+* Version:     1.0.0
+* Author:      Robin Tirole
+* Domain Path: /languages
+*/
+
